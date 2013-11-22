0108d6035f1010e931eaeb0a1f2de7c3feef1b9b
crater2150 <me@crater2150.de>
2013-11-22 10:04:14 +0100

    Refactor and document the movielist

--- lib/moviebot/bot.rb
+++ lib/moviebot/bot.rb
@@ -5,13 +5,37 @@ require 'moviebot/configuration'
 class Moviebot::Bot
   USAGE = <<-HELP
 Folgende Befehle kenne ich:
- !suggest <filmtitel>
-     Schlage einen Film vor. Existiert er schon zähle ich das als Stimme.
- !vote <filmtitel oder nummer>
-     Stimme für einen Film
+ !suggest <titel>
+     Füge einen neuen Vorschlag hinzu.
+     Existiert er schon zähle ich das als Stimme.
+ !vote <titel oder nummer>
+     Stimme für einen Vorschlag
+ !unvote <titel oder nummer>
+     Nimm eine Stimme zurück
  !list
-     Zeige die Filme sortiert nach Anzahl Stimmen an
+     Zeige die Vorschläge sortiert nach Anzahl Stimmen an
   HELP
+
+  REPLIES = {
+    existing_voted:
+      "\"%s\" wurde schon vorgeschlagen, ich zähle das als Stimme dafür",
+    existing_already_voted:
+      "\"%s\" wurde schon vorgeschlagen und du hast bereits dafür gestimmt",
+    added:
+      "\"%s\" wurde hinzugefügt",
+    no_such_movie:
+      "Ich kenne \"%s\" nicht.",
+    voted:
+      "Deine Stimme für \"%s\" wurde gezählt",
+    already_voted:
+      "Du hast bereits für \"%s\" gestimmt",
+    unvoted:
+      "Deine Stimme für \"%s\" wurde zurückgenommen",
+    no_such_vote:
+      "Du hast nicht für \"%s\" gestimmt",
+    no_movies:
+      "Noch keine Vorschläge"
+  }
 
   def initialize(config)
     movies = Moviebot.load_movie_list config[:movielist]
@@ -25,15 +49,30 @@ Folgende Befehle kenne ich:
 
 
       on :message, /!suggest (.*)/ do |m, movie|
-        movies.add_movie(m, movie)
+        message = movies.add_movie(m.user.nick, movie)
+        m.reply(REPLIES[message] % movie)
       end
 
       on :message, /!vote (.*)/ do |m, movie|
-        movies.vote(m, movie)
+        message, name = movies.vote(m.user.nick, movie)
+        m.reply(REPLIES[message] % name)
+      end
+
+      on :message, /!unvote (.*)/ do |m, movie|
+        message, name = movies.unvote(m.user.nick, movie)
+        m.reply(REPLIES[message] % name)
       end
 
       on :message, /!list/ do |m|
-        movies.list(m)
+        ranking = movies.list(m)
+
+        if ranking.empty?
+          m.reply REPLIES[:no_movies]
+        else
+          ranking.each_with_index do |movie,index|
+            m.reply ("(%2d)  " % index) +  movie.to_formatted_s
+          end
+        end
       end
 
       on :message, /!help/ do |m|
--- lib/moviebot/movielist.rb
+++ lib/moviebot/movielist.rb
@@ -2,60 +2,115 @@
 require 'set'
 
 module Moviebot
+  # Represents a list of movies sorted by votes.
   class MovieList
+    # Path for saving the list to disk
     attr_accessor :path
+    
+    # The movielist needs to know the path to its file, so it can automatically
+    # save any changes
     def initialize(path)
       @movies = SortedSet.new
       @path = path
     end
 
-    def list(m)
-      if @movies.empty?
-        m.reply "Noch keine Filme vorgeschlagen"
-        return
-      end
-
-      @movies.each_with_index do |movie,index|
-        m.reply ("(%2d)  " % index) +  movie.to_s
-      end
+    # return the movies as sorted set
+    def list
+      @movies
     end
 
-    def add_movie(m, title)
+    # Add a new movie to the list with one vote by the user who added it.
+    # If the movie already exists, a vote is added.
+    def add_movie(user, title)
       movie = Movie.new(title)
 
-      unless (existing = @movies.find {|m| m.eql? movie }).nil?
-        m.reply "\"#{title}\" wurde schon vorgeschlagen, ich zähle das als Stimme dafür"
-        existing.add_vote(m.user.nick)
+      if ! (existing = @movies.find {|m| m.eql? movie }).nil?
+        if existing.add_vote(user)
+          return :existing_voted
+        else
+          return :existing_already_voted
+        end
       else
-        m.reply "\"#{title}\" wurde hinzugefügt"
         @movies.add(movie)
-        movie.add_vote(m.user.nick)
+        movie.add_vote(user)
+        return :added
       end
+    ensure
       save!
     end
 
-    def vote(m, movie)
-      if /^\d$/.match movie
-        mov = @movies.to_a[movie.to_i]
-      else
-        mov = @movies.find {|m| m.eql? mov }
-      end
+    # Adds a vote from +user+ to a movie given either by
+    # name or by index.
+    #
+    # The method returns a symbol indicating the result and the name of the
+    # movie:
+    #  +:no_such_movie+::
+    #    No matching movie was found, no vote added
+    #  +:voted+::
+    #    The movie was found and a vote was added
+    #  +:already_voted+::
+    #    The movie was found, but the user already had voted
+    def vote(user, movie)
+      mov = find(movie)
 
       if mov.nil?
-        m.reply "Diesen Film kenne ich nicht"
+        :no_such_movie, movie
       else
-        mov.add_vote(m.user.nick)
-        m.reply "Deine Stimme für #{mov.name} wurde gezählt"
+        if mov.add_vote(user)
+          return :voted, mov.name
+        else
+          return :already_voted, mov.name
+        end
       end
+    ensure
       save!
     end
 
+    # Removes a vote from +user+ from a movie given either by name or by index.
+    #
+    # The method returns a symbol indicating the result and the name of the
+    # movie:
+    #  +:no_such_movie+::
+    #    No matching movie was found, no vote removed
+    #  +:unvoted+::
+    #    The movie was found and a vote was removed
+    #  +:no_such_vote+::
+    #    The movie was found, but it had no vote from the user
+    def unvote(user, movie)
+      mov = find(movie)
+
+      if mov.nil?
+        :no_such_movie, movie
+      else
+        if mov.delete_vote(user)
+          return :unvoted, mov.name
+        else
+          return :no_such_vote, mov.name
+        end
+      end
+    end
+
+    # write the movielist to disk.
     def save!
       File.write(@path, self.to_yaml)
     end
 
+    # Ensure, that only the movie list is saved, the path to the file itself is
+    # runtime information
     def to_yaml_properties
       [:@movies]
+    end
+
+    # Finds the first movie matching the given name, or the movie with the given
+    # index if a numeric string or a number is given.
+    # If no matching movie is found, nil is returned
+    def find(movie)
+      if /^\d$/.match movie.to_s or movie.is_a? Numeric
+        @movies.to_a[movie.to_i]
+      else
+        mov = Movie.new(movie.to_s)
+        @movies.find {|m| m.eql? mov }
+      end
     end
   end
 
@@ -68,15 +123,19 @@ module Moviebot
     end
 
     def eql?(other)
-      @name == other.name
+      @name.downcase == other.name.downcase
     end
 
     def add_vote(voter)
+      return false if @votes.include voter
       @votes.add voter
+      return true
     end
 
     def delete_vote(voter)
+      return false unless @votes.include voter
       @votes.delete voter
+      return true
     end
 
     def votes
@@ -84,6 +143,10 @@ module Moviebot
     end
 
     def to_s
+
+    end
+
+    def to_formatted_s
       "%s [%d Votes]" % [ @name, votes ]
     end
 
